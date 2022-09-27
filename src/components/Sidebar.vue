d2096ba9b52439f35b287f860ba70f237a06a752
Darren Griffin <darren.griffin@live.co.uk>
2022-09-27 22:29:31 +0200

    Styling and collapse state management

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,39 +1,227 @@
 <template>
-    <div class="sidebar">
-        <div class="sidebar__nav-group">
-            <div class="sidebar__nav-group__title">Channels</div>
-            <div class="sidebar__nav-group__nav">
-                <RouterLink to="/">Home</RouterLink>
-                <RouterLink to="/about">About</RouterLink>
+    <div class="sidebar" :class="{'sidebar--collapsed': collapsed}">
+        <div class="sidebar__nav">
+            <div class="sidebar__nav-group">
+                <div class="sidebar__nav-group__title">General</div>
+                <div class="sidebar__nav-group__nav">
+                    <RouterLink to="/" class="nav-item">
+                        <i class="bi bi-house nav-item__icon"></i>
+                        <span class="nav-item__label">Home</span>
+                    </RouterLink>
+                    <RouterLink to="/about" class="nav-item">
+                        <i class="bi bi-question-circle nav-item__icon"></i>
+                        <span class="nav-item__label">About</span>
+                    </RouterLink>
+                </div>
+            </div>
+            <div class="sidebar__nav-group">
+                <div class="sidebar__nav-group__title">Archive</div>
+                <div class="sidebar__nav-group__nav">
+                    <RouterLink to="/" class="nav-item">
+                        <i class="bi bi-camera-video nav-item__icon"></i>
+                        <span class="nav-item__label">VODs</span>
+                    </RouterLink>
+                    <RouterLink to="/about" class="nav-item">
+                        <i class="bi bi-chat-right-text nav-item__icon"></i>
+                        <span class="nav-item__label">Chat</span>
+                    </RouterLink>
+                </div>
+            </div>
+            <div class="sidebar__nav-group">
+                <div class="sidebar__nav-group__title">Archive</div>
+                <div class="sidebar__nav-group__nav">
+                    <RouterLink to="/" class="nav-item">
+                        <i class="bi bi-camera-video nav-item__icon"></i>
+                        <span class="nav-item__label">VODs</span>
+                    </RouterLink>
+                    <RouterLink to="/about" class="nav-item">
+                        <i class="bi bi-chat-right-text nav-item__icon"></i>
+                        <span class="nav-item__label">Chat</span>
+                    </RouterLink>
+                </div>
+            </div>
+            <div class="sidebar__nav-group">
+                <div class="sidebar__nav-group__title">Archive</div>
+                <div class="sidebar__nav-group__nav">
+                    <RouterLink to="/" class="nav-item">
+                        <i class="bi bi-camera-video nav-item__icon"></i>
+                        <span class="nav-item__label">VODs</span>
+                    </RouterLink>
+                    <RouterLink to="/about" class="nav-item">
+                        <i class="bi bi-chat-right-text nav-item__icon"></i>
+                        <span class="nav-item__label">Chat</span>
+                    </RouterLink>
+                </div>
+            </div>
+            <div class="sidebar__nav-group">
+                <div class="sidebar__nav-group__title">Archive</div>
+                <div class="sidebar__nav-group__nav">
+                    <RouterLink to="/" class="nav-item">
+                        <i class="bi bi-camera-video nav-item__icon"></i>
+                        <span class="nav-item__label">VODs</span>
+                    </RouterLink>
+                    <RouterLink to="/about" class="nav-item">
+                        <i class="bi bi-chat-right-text nav-item__icon"></i>
+                        <span class="nav-item__label">Chat</span>
+                    </RouterLink>
+                </div>
+            </div>
+            <div class="sidebar__nav-group">
+                <div class="sidebar__nav-group__title">Archive</div>
+                <div class="sidebar__nav-group__nav">
+                    <RouterLink to="/" class="nav-item">
+                        <i class="bi bi-camera-video nav-item__icon"></i>
+                        <span class="nav-item__label">VODs</span>
+                    </RouterLink>
+                    <RouterLink to="/about" class="nav-item">
+                        <i class="bi bi-chat-right-text nav-item__icon"></i>
+                        <span class="nav-item__label">Chat</span>
+                    </RouterLink>
+                </div>
+            </div>
+            <div class="sidebar__nav-group">
+                <div class="sidebar__nav-group__title">Archive</div>
+                <div class="sidebar__nav-group__nav">
+                    <RouterLink to="/" class="nav-item">
+                        <i class="bi bi-camera-video nav-item__icon"></i>
+                        <span class="nav-item__label">VODs</span>
+                    </RouterLink>
+                    <RouterLink to="/about" class="nav-item">
+                        <i class="bi bi-chat-right-text nav-item__icon"></i>
+                        <span class="nav-item__label">Chat</span>
+                    </RouterLink>
+                </div>
             </div>
         </div>
+        <div class="sidebar__collapse-toggle"><i class="bi bi-arrow-bar-left" @click="toggleCollapse()"></i></div>
     </div>
 </template>
+
+<script>
+export default {
+    data() {
+        return {
+            collapsed: false               
+        }
+    },
+    methods: {
+        toggleCollapse(){
+            this.collapsed = !this.collapsed
+            localStorage.setItem('sidebarCollapsed', this.collapsed ? 'true' : 'false')
+        }
+    },
+    mounted() {
+        const storedSidebarCollapsedState = localStorage.getItem('sidebarCollapsed');
+        if(storedSidebarCollapsedState && storedSidebarCollapsedState == 'true'){
+            this.collapsed = true;
+        }
+        console.log(storedSidebarCollapsedState);
+    }
+}
+</script>
 
 <style lang="scss" scoped>
 .sidebar {
     background-color: #FFFFFF;
     border-right: 1px solid rgba(0, 0, 0, 0.1);
     box-shadow: 0 0 .5rem rgba(0, 0, 0, 0.15);
-    width: 250px;
+    box-sizing: border-box;
+    display: flex;
+    flex-direction: column;
     height: calc(100vh - 56px);
-    padding-top: 1rem;
+    padding: 2rem 0 1rem 0;
+    transition: width .5s, padding .5s;
+    width: 250px;
+    overflow: hidden;
+    
+    &--collapsed {
+        padding: 1rem 0;
+        width: 56px;
+    }
 
+    &--collapsed & {
+        &__collapse-toggle {
+            i:before {
+                transform: rotate(180deg) !important;
+            }
+        }
+
+        &__nav{
+            gap: 0;
+        }
+
+        &__nav-group {
+            &__title {
+                height: 0;
+            }
+
+            &__nav {
+                .nav-item {
+                    &__label {
+                        color: transparent;
+                    }
+                }
+            }
+        }
+    }
+
+    &__collapse-toggle {
+        display: flex;
+        justify-content: flex-end;
+        padding: 0 1rem;
+        margin-top: 1rem;
+        
+        i{
+            cursor: pointer;
+        }
+    }
+    
+    &__nav {
+        display: flex;
+        flex-direction: column;
+        flex-grow: 1;
+        gap: 2rem;
+        overflow-x: hidden;
+        overflow-y: auto;
+        transition: gap .25s;
+    }
+    
     &__nav-group {
         &__title {
             color: rgba(0, 0, 0, 0.5);
             font-size: 12px;
             font-weight: 700;
+            height: 12px;
             text-transform: uppercase;
+            transition: height .25s;
+            overflow: hidden;
+            margin-bottom: .75rem;
             padding: 0 1rem;
         }
 
         &__nav {
             display: flex;
             flex-direction: column;
+            
+            .nav-item {
+                padding: .75rem 1rem;
+                white-space: nowrap;
+                transition: background-color .25s ease-in-out;
+                
+                &:hover {
+                    background-color: rgba(0, 0, 0, 0.05);
+                }
+                
+                &__label{
+                    transition: color .25s ease-in-out;
+                }
 
-            a {
-                padding: .5rem 1rem;
+                &__icon {
+                    display: inline-block;
+                    margin-right: 1rem;
+                    text-align: center;
+                    width: 1.5rem;
+                }
             }
         }
     }
