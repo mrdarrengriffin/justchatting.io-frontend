1350acd365c33f8a9595a013096fe8de4b9e4e9c
Darren Griffin <darren.griffin@live.co.uk>
2022-10-03 21:21:35 +0200

    Improve build and serve script

--- serve.js
+++ serve.js
@@ -1,19 +1,27 @@
 // Dependencies
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
 
 // Improts
-const { createServer } = require("http");
+const { createServer } = require("https");
 
+// Initialise express
 const app = express();
 
-//app.use(cors());
+// Create the https server with the certificate
+const httpServer = createServer({
+    key: fs.readFileSync("./server.key"),
+    cert: fs.readFileSync("./server.cert"),
+}, app);
 
-const httpServer = createServer(app);
-
+// Serve static files
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './dist/index.html'));
 })
+
+// Also serve assets
 app.use(express.static(path.join(__dirname, './dist')));
 
-httpServer.listen(80);
+// Listen of 443
+httpServer.listen(443);
